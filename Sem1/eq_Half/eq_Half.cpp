a8bf1d678604123eb341d9d8921d3cc588b22997
Daryazarydneva <160155089+Daryazarydneva@users.noreply.github.com>
2025-12-19 01:07:13 +0500

    Add files via upload

---
+++ Sem1/eq_Half/eq_Half.cpp
@@ -0,0 +1,30 @@
+﻿// eq_Half.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+//9 ВАРИАНТ
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+//f(x) = x-1/3+sin3,6x = 0
+// [0; 0,85]
+{
+    float a, b, c;
+    float fa, fb, fc;
+    double eps = 0.000001;
+    a = 0;
+    b = 0.85;
+
+    while (b - a > eps)
+    {
+        c = (a + b) / 2;
+        fa = (a - (1 / 3 + sin(3.6 * a)));
+        fb = (b - (1 / 3 + sin(3.6 * b)));
+        fc = (c - (1 / 3 + sin(3.6 * c)));
+
+        if (fa * fc < 0) { b = c; }
+        else if (fc * fb < 0) { a = c; }
+
+    }
+    cout << a << " " << b << endl;
+}
+
---
+++ Sem1/eq_iter/eq_Iter.cpp
@@ -0,0 +1,27 @@
+﻿// eq_Iter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//5 вариант
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+//f(x) = 0.25*pow(x, 3)+x-1.2502
+// [0, 2]
+//f'(x)=0.75*pow(x, 2)+1
+{
+    double x, xPrev;
+    double eps = 0.000001;
+    double lambda = -0.5;
+
+    x = 1;
+    xPrev = 0;
+
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = lambda * (0.25 * pow(xPrev, 3) + xPrev - 1.2502) + xPrev;
+    }
+    cout<< x <<endl;
+    return 0;
+}
+
