28370fbc0d8cfa8164d3c0aab5b4abfbf49c6534
Daryazarydneva <160155089+Daryazarydneva@users.noreply.github.com>
2026-02-21 20:04:19 +0500

    Add files via upload

---
+++ Sem2/recursii/Fibanachi/fibanachi.cpp
@@ -0,0 +1,25 @@
+﻿// fibanachi.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+    return fib(n - 2) + fib(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+        cout << fib(i)<<" ";
+    return 0;
+    
+}
