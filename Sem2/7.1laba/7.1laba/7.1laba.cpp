78adc8bc8c59b8b7583aa90a45b35fb865d98384
Darya Zarydneva <daryazarydneva@gmail.com>
2026-02-21 23:07:44 +0500

    dd

---
+++ Sem2/7.1laba/7.1laba/7.1laba.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+// Функция а) для сложения десятичных дробей (тип double)
+double dr(double a, double b) {
+    return a + b;
+    
+
+}
+
+double dr(int chisl1, int znam1, int chisl2, int znam2)
+{
+    int drob = chisl1 * znam2 + chisl2 * znam1;
+    int drob2 = znam1 * znam2;
+    return (double)drob/drob2;
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a1, b1;
+    cout << "введите десятичные дроби";
+    cin >> a1;
+    cin >> b1;
+    cout<<"результат " << dr(a1, b1)<<endl;
+    
+    double ch1, zn1, ch2, zn2;
+    cout << "введите числитель и знаменатель";
+    cin >> ch1 >> zn1;
+    cout << "введите числитель и знаменатель 2";
+    cin >> ch2 >> zn2;
+    cout << "результат " << dr(ch1, zn1, ch2, zn2);
+
+    return 0;
+}
---
+++ Sem2/7.2laba/7.2laba/7.2laba.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cstdarg>
+using namespace std;
+
+double max(int type, int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+
+    double maximum;
+    if (type == 1) {
+        maximum = va_arg(args, int);
+
+        for (int i = 1; i < count; i++) {
+            int num = va_arg(args, int);
+            if (num > maximum) { maximum = num; }
+        }
+    }
+    else {
+        maximum = va_arg(args, double);
+        for (int i = 1; i < count; i++) {
+            int num = va_arg(args, double);
+            if (num > maximum) { maximum = num; }
+        }
+
+    }
+    va_end(args);
+    return maximum;
+
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    cout << "5 элементов. int " << max(1, 6, 67, 4, 2) << endl;
+    cout << "10 элементов. double " << max(2, 10,
+        3.14, 2.71, 1.618, 0.5, 2.0,
+        4.5, 1.1, 3.3, 2.2, 0.9) << endl;
+    cout << "12 элементов. int " << max(1, 12,
+        100, 45, 67, 23, 89, 12,
+        56, 78, 34, 90, 11, 5) << endl;
+
+    return 0;
+    
+}
